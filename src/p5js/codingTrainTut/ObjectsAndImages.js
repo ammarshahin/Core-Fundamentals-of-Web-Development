c20fc9c48ae7559ed4be1b743fc6f9668040e263
AmmarMShahin <ammar.shahin@va.ai>
2021-06-16 15:15:24 +0200

    minor optmizations

--- src/p5js/codingTrainTut/ObjectsAndImages.js
+++ src/p5js/codingTrainTut/ObjectsAndImages.js
@@ -1,39 +1,41 @@
 /**
  * @file Objects and Images .js
  * @author Ammar Shahin (Ammar0Shahin@gmail.com)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2021-01-08
- * 
+ *
  * @copyright Copyright (c) 2021
- * 
+ *
  */
 let width = 600;
 let hight = 600;
 
 let array = [];
-let bubble, arraySize = 400;
+let bubble,
+  arraySize = 400;
 
-function preload()
-{
-    bubble = loadImage("src/p5js/Images/bubble.jpg");
+function preload() {
+  bubble = loadImage("src/p5js/Images/bubble.jpg");
 }
 
-function setup()
-{
-    createCanvas(width, hight);
-    for (let index = 0; index < arraySize; index++)
-    {
-        array[index] = new Bubble(random(width), random(hight), random(10, 50), [5, -5], random(50, 255));
-    }
+function setup() {
+  createCanvas(width, hight);
+  for (let index = 0; index < arraySize; index++) {
+    array[index] = new Bubble(
+      random(width),
+      random(hight),
+      random(10, 50),
+      10,
+      random(50, 255)
+    );
+  }
 }
 
-function draw()
-{
-    background(245);
-    for (let index = 0; index < arraySize; index++)
-    {
-        array[index].move();
-        array[index].show();
-    }
+function draw() {
+  background(245);
+  array.forEach((x) => {
+    x.move();
+    x.show();
+  });
 }
--- src/p5js/codingTrainTut/printAllColors.js
+++ src/p5js/codingTrainTut/printAllColors.js
@@ -1,53 +1,43 @@
 /**
  * @file printAllColors.js
  * @author Ammar Shahin (Ammar0Shahin@gmail.com)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2021-01-08
- * 
+ *
  * @copyright Copyright (c) 2021
- * 
+ *
  */
 
-var indexR = 0;
-var indexB = 0;
-var indexG = 0;
+let indexR = 0;
+let indexB = 0;
+let indexG = 0;
+let step = 10;
 
-function setup()
-{
-    createCanvas(1500, 700);
+function setup() {
+  createCanvas(1500, 700);
 }
 
-function draw()
-{
-    background(255);
-    circle(200, 200, 200);
+function draw() {
+  background(255);
+  circle(200, 200, 200);
 
-    fill(indexR, indexG, indexB);
-    console.log(indexR, indexG, indexB);
+  fill(indexR, indexG, indexB);
+  console.log(indexR, indexG, indexB);
 
-    if (indexB >= 255)
-    {
-        indexB = 0;
-        if (indexG >= 255)
-        {
-            indexG = 0;
-            if (indexR >= 255)
-            {
-                indexR = 0;
-            }
-            else
-            {
-                indexR++;
-            }
-        }
-        else
-        {
-            indexG++;
-        }
+  if (indexB >= 255) {
+    indexB = 0;
+    if (indexG >= 255) {
+      indexG = 0;
+      if (indexR >= 255) {
+        indexR = 0;
+      } else {
+        indexR += step;
+      }
+    } else {
+      indexG += step;
     }
-    else
-    {
-        indexB++;
-    }
+  } else {
+    indexB += step;
+  }
 }
