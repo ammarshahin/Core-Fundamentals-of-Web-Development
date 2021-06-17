68b53117ae4cee02f0039c586a97cf335ad697bd
AmmarMShahin <ammar.shahin@va.ai>
2021-06-17 20:12:27 +0200

    starting the oop carsh course

--- src/Traversy Media/oop.js
+++ src/Traversy Media/oop.js
@@ -23,7 +23,7 @@ class Circle {
   getRadius = () => this.radius;
   setRadius = (r) => (this.radius = r);
 
-  //! different ways to write a method
+  //* different ways to write a method
 
   getArea2 = function () {
     return 3.14 * this.radius * this.radius;
@@ -37,7 +37,34 @@ class Circle {
 }
 
 let c1 = new Circle(10);
-console.log("radius = ", c1.getRadius());
+// console.log("radius = ", c1.getRadius());
 c1.setRadius(1);
-console.log("radius = ", c1.getRadius());
-console.log("Area = ", c1.getArea());
+// console.log("radius = ", c1.getRadius());
+// console.log("Area = ", c1.getArea());
+
+//! ************************** oop crash course *******************************
+// string as primitive types
+const s1 = "hello";
+// console.log("s1 :", s1);
+// console.log("s1 type:", typeof s1);
+
+// string as an object
+const s2 = new String("ammar");
+// console.log("s2 :", s2);
+// console.log("s2 type:", typeof s2);
+
+//! ******************** object literals *********************
+
+const book1 = {
+  title: "book_one",
+  author: "author_one",
+  year: "1988",
+  getSummery: function () {
+    return `This book is titled with ${this.title}, Written by ${this.author} in ${this.year}`;
+  },
+};
+
+// console.log(book1.getSummery());
+// console.log(Object.values(book1));
+// console.log(Object.keys(book1));
+//
