b982e028cd5c0eb3311cb36a800fc17bc86bdbf9
ammarshahin <ammar0ammar612@gmail.com>
2021-06-18 22:49:38 +0200

    finishing the oop crash course

---
+++ src/Elzero Bootcamp/main.js
@@ -0,0 +1,23 @@
+//! setting up an alert
+// window.alert("ammar is here");
+
+//! writing to document
+//! we can write anything that can be written to the html file
+// document.write("<h1> Title </h1>");
+
+//! console
+// console.log("this is just a message");
+// console.error("this is an error");
+// console.warn("this is a warning");
+// console.table(["ammar", "mohamed", "shahin"]);
+
+//! formatting to the console text
+
+// console.log(
+//   "this the origin font %cand this is the formatted %cfont",
+//   "color : red; font-size: 12px",
+//   "color : blue; font-size: 12px"
+// );
+
+
+console.log();
--- src/Traversy Media/oop.js
+++ src/Traversy Media/oop.js
@@ -67,4 +67,53 @@ const book1 = {
 // console.log(book1.getSummery());
 // console.log(Object.values(book1));
 // console.log(Object.keys(book1));
-//
+
+//! ************************************* ES6 OOP *********************
+class Book {
+  constructor(title, author, year) {
+    //* note that we are able to define new attributes to the class dynamically
+    this.title = title;
+    this.author = author;
+    this.year = year;
+  }
+
+  getSummery() {
+    return `This book is titled with ${this.title}, Written by ${this.author} in ${this.year}`;
+  }
+
+  getAge() {
+    return new Date().getFullYear() - this.year;
+  }
+
+  revise(newYear) {
+    this.year = newYear;
+    this.revised = true;
+  }
+
+  //! static methods
+  //* static methods let you make an abstract class that you can invoke it's static methods without making any objects of it
+  static topBookStore() {
+    return "book_store";
+  }
+}
+
+const b1 = new Book("book_class", "ammar", "2000");
+// console.log("b1 :", b1);
+// console.log("b1 summery:", b1.getSummery());
+// console.log(`this book is ${b1.getAge()} years old`);
+// b1.revise("2018");
+// console.log("b1 :", b1);
+
+//! static methods
+// console.log(Book.topBookStore());
+
+//! inhertance
+class Magazine extends Book {
+  constructor(title, author, year, month) {
+    super(title, author, year);
+    this.month = month;
+  }
+}
+
+const mag1 = new Magazine("book_class", "ammar", "2000", "jan");
+console.log("mag1 :", mag1);
